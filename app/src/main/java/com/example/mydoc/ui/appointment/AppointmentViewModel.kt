cac90fab54dcb389e495410fe0a41f7b1c576f7c
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-09-27 22:28:21 +0300

    Implemented doctor view model to fetch data from backend api

--- app/src/main/java/com/example/mydoc/ui/appointment/AppointmentViewModel.kt
+++ app/src/main/java/com/example/mydoc/ui/appointment/AppointmentViewModel.kt
@@ -1,11 +1,27 @@
 package com.example.mydoc.ui.appointment
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mydoc.Appointments.Appointment
-import com.example.mydoc.Doctors.Doctor
+import com.example.mydoc.repository.MyDocRepository
+import kotlinx.coroutines.launch
 import com.example.mydoc.R
+import com.example.mydoc.network.Resource
 
 class AppointmentViewModel : ViewModel() {
+
+
+    private val _appointments = MutableLiveData<Resource<List<Appointment>>>()
+    val appointments: LiveData<Resource<List<Appointment>>> get() = _appointments
+
+    fun fetchAppointments() {
+        _appointments.postValue(Resource.Loading())
+        viewModelScope.launch {
+//            _appointments.postValue(repository.getAppointments())
+        }
+    }
 
     private val allAppointments = listOf(
         Appointment("Dr. Ochieng Oloo", "Surgeon",  R.drawable.ochieng),
